62b6534c3d6133a1dc18b9e68693c5102388f36f
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-04-09 18:54:58 +0000

    Create FullTime Class

---
+++ emplcentr/src/main/java/org/vntu/emplcentr/FullTime.java
@@ -0,0 +1,34 @@
+package org.vntu.emplcentr;
+
+public class FullTime extends GenInform {
+
+  private String mainWork;
+  private String expirience;
+  private String speciality;
+
+  public String getMainWork() {
+    return mainWork;
+  }
+
+  public void setMainWork(String mainWork) {
+    this.mainWork = mainWork;
+  }
+
+  public String getExpirience() {
+    return expirience;
+  }
+
+  public void setExpirience(String expirience) {
+    this.expirience = expirience;
+  }
+
+  public String getSpeciality() {
+    return speciality;
+  }
+
+  public void setSpeciality(String speciality) {
+    this.speciality = speciality;
+  }
+
+}
+
