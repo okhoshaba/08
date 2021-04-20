e659bab70e488d867154215b652f61742fdc58a8
okhoshaba <56321983+okhoshaba@users.noreply.github.com>
2021-04-20 19:15:36 +0300

    Add some Classes

---
+++ emplcentr/src/main/java/org/vntu/emplcentr/Course.java
@@ -0,0 +1,16 @@
+package org.vntu.emplcentr;
+
+public class Course extends GenInform {
+
+  private String paid;
+
+  public String getPaid() {
+    return paid;
+  }
+
+  public void setPaid(String paid) {
+    this.paid = paid;
+  }
+
+}
+
---
+++ emplcentr/src/main/java/org/vntu/emplcentr/OneTime.java
@@ -0,0 +1,25 @@
+package org.vntu.emplcentr;
+
+public class OneTime extends GenInform {
+
+  private String relatedWork;
+  private String period;
+
+  public String getRelatedWork() {
+    return relatedWork;
+  }
+
+  public void setRelatedWork(String relatedWork) {
+    this.relatedWork = relatedWork;
+  }
+
+  public String getPeriod() {
+    return period;
+  }
+
+  public void setPeriod(String period) {
+    this.period = period;
+  }
+
+}
+
